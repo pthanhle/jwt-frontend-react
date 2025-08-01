cf71663051f7ae5f26bc5ba8d5a363e4e5e6f803
pthanhle <22110228@student.hcmute.edu.vn>
2025-08-01 13:49:51 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import Nav from './components/Navigation/Nav';
 import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app-container'>
+        {/* <Nav /> */}
+        <Switch>
+          <Route path="/news">
+            news
+          </Route>
+          <Route path="/about">
+            about
+          </Route>
+          <Route path="/contact">
+            contact
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/" exact>
+            home
+          </Route>
+          <Route path="*">
+            404 NOT FOUND
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,10 @@
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            login
+            <button className="btn btn-primary">test</button>
+        </div>
+    )
+}
+
+export default Login;
