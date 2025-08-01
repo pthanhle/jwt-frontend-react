41fa4f4a1fdd4e1363efc40e21873659647c0c1d
pthanhle <22110228@student.hcmute.edu.vn>
2025-08-01 14:25:50 +0700

    Login UI

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,30 @@
+import './Login.scss'
+
 const Login = (props) => {
     return (
-        <div className="login-container">
-            login
-            <button className="btn btn-primary">test</button>
+        <div className="login-container mt-3">
+            <div className="container">
+                <div className="row">
+                    <div className="content-left col-7">
+                        <div className='brand'>
+                            Facebook
+                        </div>
+                        <div className='detail'>
+                            Facebook giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống của bạn.
+                        </div>
+                    </div>
+                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                        <input type='text' className='form-control' placeholder='Email address or phone number' />
+                        <input type='password' className='form-control' placeholder='Password' />
+                        <button className='fb-login-btn'>Log In</button>
+                        <span className='text-center'>Forgot your password?</span>
+                        <hr />
+                        <div className='text-center'>
+                            <button className='fb-create-btn'>Create New Account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
