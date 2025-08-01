4519993e99683f0a15dab5c960f2e2bda7fe282b
pthanhle <22110228@student.hcmute.edu.vn>
2025-08-01 15:40:40 +0700

    done login UI

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,22 +2,27 @@ import './Login.scss'
 
 const Login = (props) => {
     return (
-        <div className="login-container mt-3">
+        <div className="login-container">
             <div className="container">
-                <div className="row">
-                    <div className="content-left col-7">
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-12 d-none col-sm-7 d-sm-block">
                         <div className='brand'>
-                            Facebook
+                            facebook
                         </div>
                         <div className='detail'>
                             Facebook giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống của bạn.
                         </div>
                     </div>
-                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                    <div className='logo d-sm-none'>
+                        facebook
+                    </div>
+                    <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
                         <input type='text' className='form-control' placeholder='Email address or phone number' />
                         <input type='password' className='form-control' placeholder='Password' />
                         <button className='fb-login-btn'>Log In</button>
-                        <span className='text-center'>Forgot your password?</span>
+                        <span className='text-center'>
+                            <a className="forgot-password" href='#'>Forgot your password?</a>
+                        </span>
                         <hr />
                         <div className='text-center'>
                             <button className='fb-create-btn'>Create New Account</button>
